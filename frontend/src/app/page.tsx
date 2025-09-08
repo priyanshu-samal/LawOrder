a6e1458e463c75f55cd223ed323c13c992f1b5ea
Priyanshu samal <samalpriyanshu966@gmail.com>
2025-09-08 08:22:29 +0530

    f

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0';
 import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
 
--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,12 +1,29 @@
-"use client";
+'use client';
 
-import { ChakraProvider } from "@chakra-ui/react";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Auth0Provider } from '@auth0/nextjs-auth0';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f7fafc",
+      100: "#edf2f7",
+      200: "#e2e8f0",
+      300: "#cbd5e0",
+      400: "#a0aec0",
+      500: "#718096",
+      600: "#4a5568",
+      700: "#2d3748",
+      800: "#1a202c",
+      900: "#171923",
+    },
+  },
+});
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </UserProvider>
+    <Auth0Provider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </Auth0Provider>
   );
 }
